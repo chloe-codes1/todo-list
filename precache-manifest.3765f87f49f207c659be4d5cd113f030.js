707c0ac11e5572e208ada728fc24e1563bc65491
chloe-codes1 <juhyun.kim@lindsey.edu>
2019-12-09 01:47:39 +0900

    Updates

---
+++ precache-manifest.3765f87f49f207c659be4d5cd113f030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e847128858b3dc311347e0019a4425a",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "3dfb4afba4769262bcec",
+    "url": "/todo-list/static/css/main.39328af0.chunk.css"
+  },
+  {
+    "revision": "92e1f6dab70ce2e628c4",
+    "url": "/todo-list/static/js/2.cbf5566b.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/todo-list/static/js/2.cbf5566b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3dfb4afba4769262bcec",
+    "url": "/todo-list/static/js/main.7694ca77.chunk.js"
+  },
+  {
+    "revision": "06cf3d4e48698e24c843",
+    "url": "/todo-list/static/js/runtime-main.8a3463ed.js"
+  }
+]);
